bb267caecd2f1e358c6de960d99bcf66f7516b80
Zacharia <zacmwangi94@gmail.com>
2018-07-04 11:25:12 +0300

    (feature) Enable adding of homes and linking them to host

--- contracts/HomeListing.sol
+++ contracts/HomeListing.sol
@@ -1,8 +1,21 @@
 pragma solidity ^0.4.0;
 
+// @title HomeListing
 contract HomeListing {
+
+    struct Home {
+        uint id;
+        string physicalAddress;
+    }
+
+    mapping (address => Home) hostToHome;
 
     constructor() {
 
+    }
+
+
+    function addHome(string physicalAddress) public {
+        hostToHome[msg.sender] = Home(uint(keccak256(physicalAddress, msg.sender)), physicalAddress);
     }
 }
