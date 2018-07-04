20efef4c54f8a54c365fa8ab3a4fd64dc9702f7a
Zacharia <zacmwangi94@gmail.com>
2018-07-04 12:59:10 +0300

    (feature) Enable fetching listings by physical address

--- contracts/HomeListing.sol
+++ contracts/HomeListing.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.21;
 
 // @title HomeListing
 contract HomeListing {
@@ -7,15 +7,32 @@ contract HomeListing {
         uint id;
         string physicalAddress;
     }
-
+    Home [] public homes;
     mapping (address => Home) hostToHome;
-
+    event HomeEvent(uint []);
     constructor() {
 
     }
 
+    //@param physicalAddress - the actual address of the home a host wants to list (not the ethereum address)
+    function addHome(string _physicalAddress) public {
+        uint _id = uint(keccak256(_physicalAddress, msg.sender));
+        Home memory home = Home(_id, _physicalAddress);
+        hostToHome[msg.sender] = home;
+        homes.push(home);
+    }
 
-    function addHome(string physicalAddress) public {
-        hostToHome[msg.sender] = Home(uint(keccak256(physicalAddress, msg.sender)), physicalAddress);
+    //@param physicalAddress - the actual address of the home a host wants to list (not the ethereum address)
+    //@return _id - list of ids for homes
+    function listHomesByAddress(string _physicalAddress) public returns(uint [] _id ) {
+        uint [] results;
+        for(uint i = 0 ; i<homes.length; i++) {
+            if(keccak256(homes[i].physicalAddress) == keccak256(_physicalAddress)) {
+                results.push(homes[i].id);
+            }
+        }
+        HomeEvent(results);
+        return results;
+
     }
 }
